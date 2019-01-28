292d9a8a19c982447c5a343e1684e7a3d24b8b91
Peter <petro004>
2019-01-28 21:02:41 +0200

    Commit Game TicTacToe

---
+++ src/Game/GameField.java
@@ -0,0 +1,8 @@
+package Game;
+
+
+public class GameField {
+    public static final char USER = 'X';
+    public static final char COMPUTER = 'O';
+    public static final char mas[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+}
