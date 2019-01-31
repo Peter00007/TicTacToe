08d8a50c47ecd453f3b84f870ea3b9f7596726cf
Peter <petro004>
2019-01-31 17:39:06 +0200

    Commit update game vesion 2.0

--- src/main/java/com/tictactoe/Field.java
+++ src/main/java/com/tictactoe/Field.java
@@ -2,9 +2,13 @@ package main.java.com.tictactoe;
 
 
 public class Field {
-    private final char mas[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+    private char mas[] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
 
     public char[] getMas() {
         return mas;
+    }
+
+    public void setMas(char[] mas) {
+        this.mas = mas;
     }
 }
--- src/main/java/com/tictactoe/GameLogic.java
+++ src/main/java/com/tictactoe/GameLogic.java
@@ -3,35 +3,57 @@ package main.java.com.tictactoe;
 import java.util.Random;
 import java.util.Scanner;
 
-public class GameLogic extends Field {
+public class GameLogic {
+    Field field = new Field();
     Scanner in = new Scanner(System.in);
     private final char COMPUTER = 'O';
     private final char USER = 'X';
+    static final String masOfChar[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private final String choiceFirstMove = "Хто ходитиме першим: Ви(натисніть 1) чи Комп'ютер(натисніть 2)?";
+    private final String winUser = "Переміг гравець";
+    private final String winComp = "Переміг компютер";
+    private final String isDraw = "Нічия";
+    private final String incorrectlyEnteredData = "Невірно введені дані";
+    private final String enterCell = "Введіть число від 1 до 9";
     String validInputValues;
-    static String masOfChar[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    int random;
+    boolean flag;
+    int cell;
+    int inputNumber;
+    int checkCell;
+    boolean isWinner;
+
+    GameLogic() {
+        validInputValues = " ";
+        random = -1;
+        flag = false;
+        cell = -1;
+        inputNumber = -1;
+        checkCell = -1;
+        isWinner = false;
+    }
 
     //random selection of cells by computer
     char randCount() {
-        int random;
         do {
-            random = new Random().nextInt(getMas().length);
-        } while (getMas()[random] != '-');
-        getMas()[random] = COMPUTER;
-        return getMas()[random];
+            random = new Random().nextInt(field.getMas().length);
+        } while (field.getMas()[random] != '-');
+        field.getMas()[random] = COMPUTER;
+        return field.getMas()[random];
 
     }
 
     //input our mas of data
     void inputMas() {
-        for (int i = 0; i < getMas().length; i++) {
-            System.out.print(getMas()[i] + " ");
+        for (int i = 0; i < field.getMas().length; i++) {
+            System.out.print(field.getMas()[i] + " ");
         }
     }
 
     //validation check for data entry
     boolean checkValid() {
         validInputValues = in.next();
-        boolean flag = false;
+        flag = false;
         for (int i = 0; i < masOfChar.length; i++) {
             flag = new String(validInputValues).equals(masOfChar[i]);
             if (flag)
@@ -46,8 +68,7 @@ public class GameLogic extends Field {
     //validation check for data entry and empty cell
     boolean checkIsEmpty() {
         validInputValues = in.next();
-        boolean flag = false;
-        int cell = -1;
+        flag = false;
         for (int i = 0; i < masOfChar.length; i++) {
             flag = new String(validInputValues).equals(masOfChar[i]);
             if (flag) {
@@ -56,7 +77,7 @@ public class GameLogic extends Field {
             }
         }
         if ((flag) && cell <= 9 && cell >= 0) {
-            if (getMas()[cell - 1] == '-') {
+            if (field.getMas()[cell - 1] == '-') {
                 return true;
             }
             return false;
@@ -66,29 +87,29 @@ public class GameLogic extends Field {
 
     //check if any of the players has won
     boolean checkWinner() {
-        if ((getMas()[0] == getMas()[1]) && (getMas()[1] == getMas()[2]) &&
-                (getMas()[0] != '-') && (getMas()[1] != '-') && (getMas()[2] != '-'))
+        if ((field.getMas()[0] == field.getMas()[1]) && (field.getMas()[1] == field.getMas()[2]) &&
+                (field.getMas()[0] != '-') && (field.getMas()[1] != '-') && (field.getMas()[2] != '-'))
             return true;
-        else if ((getMas()[3] == getMas()[4]) && (getMas()[4] == getMas()[5]) &&
-                (getMas()[3] != '-') && (getMas()[4] != '-') && (getMas()[5] != '-'))
+        if ((field.getMas()[3] == field.getMas()[4]) && (field.getMas()[4] == field.getMas()[5]) &&
+                (field.getMas()[3] != '-') && (field.getMas()[4] != '-') && (field.getMas()[5] != '-'))
             return true;
-        else if ((getMas()[6] == getMas()[7]) && (getMas()[7] == getMas()[8]) &&
-                (getMas()[6] != '-') && (getMas()[7] != '-') && (getMas()[8] != '-'))
+        if ((field.getMas()[6] == field.getMas()[7]) && (field.getMas()[7] == field.getMas()[8]) &&
+                (field.getMas()[6] != '-') && (field.getMas()[7] != '-') && (field.getMas()[8] != '-'))
             return true;
-        else if ((getMas()[0] == getMas()[3]) && (getMas()[3] == getMas()[6]) &&
-                (getMas()[0] != '-') && (getMas()[3] != '-') && (getMas()[6] != '-'))
+        if ((field.getMas()[0] == field.getMas()[3]) && (field.getMas()[3] == field.getMas()[6]) &&
+                (field.getMas()[0] != '-') && (field.getMas()[3] != '-') && (field.getMas()[6] != '-'))
             return true;
-        else if ((getMas()[1] == getMas()[4]) && (getMas()[4] == getMas()[7]) &&
-                (getMas()[1] != '-') && (getMas()[4] != '-') && (getMas()[7] != '-'))
+        if ((field.getMas()[1] == field.getMas()[4]) && (field.getMas()[4] == field.getMas()[7]) &&
+                (field.getMas()[1] != '-') && (field.getMas()[4] != '-') && (field.getMas()[7] != '-'))
             return true;
-        else if ((getMas()[2] == getMas()[5]) && (getMas()[5] == getMas()[8]) &&
-                (getMas()[2] != '-') && (getMas()[5] != '-') && (getMas()[8] != '-'))
+        if ((field.getMas()[2] == field.getMas()[5]) && (field.getMas()[5] == field.getMas()[8]) &&
+                (field.getMas()[2] != '-') && (field.getMas()[5] != '-') && (field.getMas()[8] != '-'))
             return true;
-        else if ((getMas()[0] == getMas()[4]) && (getMas()[4] == getMas()[8]) &&
-                (getMas()[0] != '-') && (getMas()[4] != '-') && (getMas()[8] != '-'))
+        if ((field.getMas()[0] == field.getMas()[4]) && (field.getMas()[4] == field.getMas()[8]) &&
+                (field.getMas()[0] != '-') && (field.getMas()[4] != '-') && (field.getMas()[8] != '-'))
             return true;
-        else if ((getMas()[6] == getMas()[4]) && (getMas()[4] == getMas()[2]) &&
-                (getMas()[6] != '-') && (getMas()[4] != '-') && (getMas()[2] != '-'))
+        if ((field.getMas()[6] == field.getMas()[4]) && (field.getMas()[4] == field.getMas()[2]) &&
+                (field.getMas()[6] != '-') && (field.getMas()[4] != '-') && (field.getMas()[2] != '-'))
             return true;
         else
             return false;
@@ -96,42 +117,39 @@ public class GameLogic extends Field {
 
     //method with start of game
     public void start() {
-        int inputNumber = -1;
-        int checkCell = -1;
         //the choice who goes first
         do {
-            System.out.println("Хто ходитиме першим: Ви(натисніть 1) чи Комп'ютер(натисніть 2)?");
+            System.out.println(choiceFirstMove);
             if (checkValid()) {
                 inputNumber = Integer.parseInt(validInputValues);
                 if (inputNumber != 1 && inputNumber != 2)
-                    System.out.println("Невірно введені дані");
+                    System.out.println(incorrectlyEnteredData);
             } else {
                 inputNumber = -1;
-                System.out.println("Невірно ввели дані");
+                System.out.println(incorrectlyEnteredData);
             }
         } while (inputNumber != 1 && inputNumber != 2);
-        int counter = getMas().length;
-        boolean f = false;
+        int counter = field.getMas().length;
         //if you choose 1, you will be the first player
         if (inputNumber == 1) {
             for (int j = 0; j < counter; j++) {
                 if (j % 2 == 0) {
-                    System.out.println("Введіть число від 1 до 9");
+                    System.out.println(enterCell);
                     //validation check for data entry and empty cell and and launching the loop for correct input
                     do {
                         checkCell = -1;
                         if (checkIsEmpty()) {
                             checkCell = Integer.parseInt(validInputValues);
                         } else
-                            System.out.println("Невірно введені дані");
+                            System.out.println(incorrectlyEnteredData);
                     } while (checkCell < 0);
-                    getMas()[checkCell - 1] = USER;
+                    field.getMas()[checkCell - 1] = USER;
                     //Checking for the end of the game
                     if (checkWinner()) {
                         inputMas();
                         System.out.println();
-                        System.out.println("Переміг гравець");
-                        f = true;
+                        System.out.println(winUser);
+                        isWinner = true;
                         break;
                     }
                 }
@@ -141,8 +159,8 @@ public class GameLogic extends Field {
                     System.out.println();
                     if (checkWinner()) {
                         System.out.println();
-                        System.out.println("Переміг комп");
-                        f = true;
+                        System.out.println(winComp);
+                        isWinner = true;
                         break;
                     }
                 }
@@ -158,33 +176,33 @@ public class GameLogic extends Field {
                     //Checking for the end of the game
                     if (checkWinner()) {
                         System.out.println();
-                        System.out.println("Переміг комп");
-                        f = true;
+                        System.out.println(winComp);
+                        isWinner = true;
                         break;
                     }
                 }
                 if (j % 2 != 0) {
-                    System.out.println("Введіть число від 1 до 9");
+                    System.out.println(enterCell);
                     do {
                         checkCell = -1;
                         if (checkIsEmpty()) {
                             checkCell = Integer.parseInt(validInputValues);
                         } else
-                            System.out.println("Невірно введені дані");
+                            System.out.println(incorrectlyEnteredData);
                     } while (checkCell < 0);
-                    getMas()[checkCell - 1] = USER;
+                    field.getMas()[checkCell - 1] = USER;
                     if (checkWinner()) {
                         inputMas();
                         System.out.println();
-                        System.out.println("Переміг гравець");
-                        f = true;
+                        System.out.println(winUser);
+                        isWinner = true;
                         break;
                     }
                 }
             }
         }
         //check for a draw
-        if (f == false)
-            System.out.println("Нічия!");
+        if (isWinner == false)
+            System.out.println(isDraw);
     }
 }
